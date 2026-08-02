a456aefa4919174c38ef0b675f74961dbcca8295
ShashankCargoRun <shashank.cargo.run@gmail.com>
2026-08-02 17:40:52 +0000

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+/*
 #![allow(unused)]
 
 use axum::{
@@ -28,4 +29,23 @@ async fn handler_hello() -> impl IntoResponse {
 
     Html("Hello <strong>World!!!</strong>")
 }
+    */
+
+
+    ///////////askama tutorial
+
+
+    use axum_askama_tutorial::routes;
+
+#[tokio::main]
+async fn main() {
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
+        .await
+        .unwrap();
+
+    let app = routes::router();
+
+    axum::serve(listener, app).await.unwrap();
+}
+
 
