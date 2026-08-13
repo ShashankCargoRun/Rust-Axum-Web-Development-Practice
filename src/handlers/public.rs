75d3c1af75ba46acb8eb8a45ae762bbdd6ecc446
ShashankCargoRun <shashank.cargo.run@gmail.com>
2026-08-13 17:51:14 +0000

    Initial commit

--- src/handlers/public.rs
+++ src/handlers/public.rs
@@ -1,9 +1,13 @@
-use crate::models::templates::HomeTemplate;
+use super::errors::AppError;
+use crate::models::{app::CurrentUser, templates::HomeTemplate};
 use askama::Template;
-use axum::response::{Html, IntoResponse, Response};
+use axum::response::{Extension, Html, IntoResponse, Response};
 
-pub async fn home() -> Response {
-    let html_string = HomeTemplate {}.render().unwrap();
+pub async fn home(Extension(current_user): Extension<CurrentUser>) -> Result<Response, AppError> {
+    let html_string = HomeTemplate {
+        is_authenticated: current_user.is_authenticated,
+    }
+    .render()?;
 
-    Html(html_string).into_response()
+    Ok(Html(html_string).into_response())
 }
